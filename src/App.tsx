e3cea16ae7483a44f30c7b68fba6714161d3eb24
pipopotamasu <ec0156hx@gmail.com>
2020-07-23 15:27:55 +0900

    Form (#3)
    * rm files
    
    * form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from 'react';
+import { useForm, useFieldArray } from 'react-hook-form';
+
+type User = {
+  name: string,
+  email: string
+}
+
+type FormType = {
+  users: User[]
+}
 
 function App() {
+  const { control, register, handleSubmit } = useForm<FormType>({
+    mode: 'onChange',
+    defaultValues: { users: [{ name: '', email: '' }] }
+  });
+
+  const { fields, append, remove } = useFieldArray<User>({
+    control,
+    name: 'users',
+  })
+
+  const appendRow = useCallback(() => {
+    append({ name: '', email: '' });
+  }, [append])
+
+  const removeRow = useCallback((index: number) => {
+    remove(index);
+  }, [remove]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(data => console.log(data))}>
+      <ul>
+        {
+          fields.map((user, i) => (
+            <li key={user.id}>
+              <input name={`users[${i}].name`} type="text" defaultValue={user.name} ref={register()} />
+              <input name={`users[${i}].email`} type="text" defaultValue={user.email} ref={register()} />
+              <button type="button" onClick={() => removeRow(i)}>remove</button>
+            </li>
+          ))
+        }
+      </ul>
+      <button type="button" onClick={appendRow}>add</button>
+      <button type="submit">submit</button>
+    </form>
   );
 }
 
